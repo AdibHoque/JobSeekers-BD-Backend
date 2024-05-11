1e51b7cba6820f474527822af0a19e7697f38a22
AdibHoque <adibhoque04@gmail.com>
2024-05-11 20:44:21 +0600

    Added GET /jobs to fetch all jobs data

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ async function run() {
   try {
     // Connect the client to the server(optional starting in v4.7)
     await client.connect();
+
+    const jobCollection = client.db('JobSeekers').collection('jobs');
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
